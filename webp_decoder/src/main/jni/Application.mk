cfe07f31bffb332bfc1ebc543fbff65480dbdac9
haoxiqiang <haoxiqiang@live.com>
2021-11-02 17:27:49 +0800

    upgrade libwebp v1.2.0->v1.2.1

--- webp_decoder/src/main/jni/Application.mk
+++ webp_decoder/src/main/jni/Application.mk
@@ -1,7 +1,6 @@
 APP_BUILD_SCRIPT := Android.mk
 
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_PLATFORM := android-16
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)
