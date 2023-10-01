5f80cff7299b0da401bf8a8642cb9147e7b65339
zjupure <zjupure@gmail.com>
2023-10-01 18:34:55 +0800

    add okhttp deps and allow http cleartext for Android P

--- webpsample/src/main/java/com/bumptech/glide/samples/webp/WebpModule.java
+++ webpsample/src/main/java/com/bumptech/glide/samples/webp/WebpModule.java
@@ -5,7 +5,13 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by liuchun on 2017/10/17.
@@ -15,7 +21,8 @@ public class WebpModule extends AppGlideModule {
 
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
-
+        //OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+        //registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
     }
 
     @Override
